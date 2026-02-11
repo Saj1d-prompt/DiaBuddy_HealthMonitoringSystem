e428bdb8e0d5922b7d79b045d5e3958337f26d0a
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-11 19:30:39 +0600

    Feature cards of Home

--- frontend/src/Component/Home Page/Home.jsx
+++ frontend/src/Component/Home Page/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../Common/Footer'
 import Header from './Header'
 import HeroSection from './HeroSection'
+import Features from './Features'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Features />
       <Footer />
     </div>
   )
