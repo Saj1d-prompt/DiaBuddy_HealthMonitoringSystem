30871b995787f31d214e8a6d04484d51b06e1f2d
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-11 19:56:32 +0600

    making Home page responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-
 import { Link } from 'react-router-dom'
 import Home from './Component/Home Page/Home'
+import CreateAccount from './Component/Login and Registration/CreateAccount'
 function App() {
   return (
     <>
       <Home />
+      {/* <CreateAccount /> */}
     </>
   )
 }
