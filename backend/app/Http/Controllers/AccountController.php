88d6c68b29af149267ea479a96055d1e5b22b786
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-12 10:38:48 +0600

    Create Account API

---
+++ backend/app/Http/Controllers/AccountController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AccountController extends Controller
+{
+    public function createAccount(Request $request){
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'date_of_birth' => 'required|date',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                'status'=> 400,
+                'errors'=>$validate->errors()
+            ], 400);
+        }
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->date_of_birth = $request->date_of_birth;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'status' => 200
+        ], 200);
+    }
+}
