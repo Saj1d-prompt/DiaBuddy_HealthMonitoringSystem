5706aeb13b5cb4fd12783f5104b9bb3419491bed
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-12 15:00:58 +0600

    Login API Route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AccountController;
 
+
+Route::post('/login', [AccountController::class, 'login']);
+
 Route::post('/register', [AccountController::class, 'createAccount']);
 
 
