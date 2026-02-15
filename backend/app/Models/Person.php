47061ec729dfa7759c6e60e70d9c48db12819230
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-15 12:15:59 +0600

    Modified Model Class and API Route

--- backend/app/Models/Person.php
+++ backend/app/Models/Person.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    protected $table = 'person';
+    protected $fillable = [
+        'number',
+        'gender',
+        'address',
+        'height',
+        'weight',
+        'blood_group',
+        'diabetes_type',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,11 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\PersonalInfoController;
 
 
 Route::post('/login', [AccountController::class, 'login']);
 
 Route::post('/register', [AccountController::class, 'createAccount']);
+
+Route::post('/personalInfo', [PersonalInfoController::class, 'storeInfo']);
 
 
 Route::get('/user', function (Request $request) {
