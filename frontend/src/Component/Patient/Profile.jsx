f58914317e51ace485d1b876f12b3d8fc47dc159
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-16 14:37:22 +0600

    Personal Information Profile Frontend

--- frontend/src/Component/Patient/Profile.jsx
+++ frontend/src/Component/Patient/Profile.jsx
@@ -12,6 +12,33 @@ const Profile = () => {
           </div>
           <button className={styles.editButton}>Edit Profile</button>
         </div>
+
+        <form action="">
+          <h3 className={styles.sectionTitle}>Personal Information</h3>
+          <div className={styles.grid}>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="">Phone Number</label>
+              <input type="text" name='number' />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="">Gender</label>
+              <select name="gender" id="gender">
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="">Address</label>
+              <textarea type="text" name='address' rows="3" />
+            </div>
+
+          </div>
+        </form>
       </div>
     </div>
   )
