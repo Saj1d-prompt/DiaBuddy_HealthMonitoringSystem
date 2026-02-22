3e1befa34f028681e54f1bceda67c4d70332e4a4
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-22 15:57:27 +0600

    Basic Structure

---
+++ frontend/src/Component/Patient/UploadReports.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../Style/UploadReports.module.css'
+
+const UploadReports = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UploadReports
