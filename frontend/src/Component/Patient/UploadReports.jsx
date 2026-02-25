a5b941a1b3ec12c8e529d64645a7fdd53704c540
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-25 14:53:30 +0600

    updated frontend for Upload report

--- frontend/src/Component/Patient/UploadReports.jsx
+++ frontend/src/Component/Patient/UploadReports.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import styles from '../../Style/UploadReports.module.css'
+import styles from '../../Style/UploadReport.module.css'
 
 const UploadReports = () => {
   return (
     <div>
-      
+      <h2>Medical Reports</h2>
+      <p>Upload your Medical Reports Here</p>
+
+      <form action="">
+        <div>
+          <p><strong>Click to upload</strong> or drag and drop</p>
+          <p>PDF, PNG, or JPG (Max 5MB)</p>
+          <input type="file" accept=".pdf, .png, .jpg, .jpeg" />
+          <button type="submit">Upload</button>
+          
+        </div>
+      </form>
     </div>
   )
 }
