809927a779a35ea78302d42e7d93f3af1ba45b66
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-02-26 15:40:08 +0600

    Validating backend

--- backend/app/Http/Controllers/MedicalReportController.php
+++ backend/app/Http/Controllers/MedicalReportController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use App\Models\MedicalReport;
 
 class MedicalReportController extends Controller
 {
-    //
+    public function storeReport(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'report_file' => 'required|mimes:pdf,jpg,jpeg,png|max:5120',
+            'report_type' => 'required|string',
+            'test_date'   => 'required|date',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        
+    }
 }
