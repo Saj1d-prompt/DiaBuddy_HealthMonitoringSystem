fc800cea31894bca54cd0dc2ada63b2d9d28c7a7
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-02 15:31:51 +0600

    Making Container

--- frontend/src/Component/Patient/ViewReport.jsx
+++ frontend/src/Component/Patient/ViewReport.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import styles from '../../Style/ViewReport.module.css';
+import styles from '../../Style/ViewReports.module.css';
 
 const ViewReport = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>View Medical Reports History</h1>
+
+        </div>
+    )
 }
 
 export default ViewReport
