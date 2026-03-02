2d2731580d7f7ca10210f430e50c3e11254fdc69
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-02 15:36:41 +0600

    Making Table for VR

--- frontend/src/Component/Patient/ViewReport.jsx
+++ frontend/src/Component/Patient/ViewReport.jsx
@@ -5,6 +5,19 @@ const ViewReport = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>View Medical Reports History</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Test Date</th>
+                            <th>Report Type</th>
+                            <th>Laboratory/Hospital</th>
+                            <th>Comments</th>
+                            <th>Report</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
 
         </div>
     )
