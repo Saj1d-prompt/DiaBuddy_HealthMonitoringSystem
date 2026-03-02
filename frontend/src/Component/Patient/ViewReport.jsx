c4693719a0157ce3ebda14bcf80c18723b226b5b
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-02 15:26:35 +0600

    Starting VR Frontend

---
+++ frontend/src/Component/Patient/ViewReport.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../Style/ViewReport.module.css';
+
+const ViewReport = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewReport
