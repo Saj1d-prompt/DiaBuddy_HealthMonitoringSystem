907914f93d1213a98f7e7792da214b07430438a2
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-06 14:24:50 +0600

    Created Heading and container

--- frontend/src/Component/Admin/AdminDashboard.jsx
+++ frontend/src/Component/Admin/AdminDashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import UserList from './UserList';
+import CreateDoctor from './CreateDoctor';
 
 const AdminDashboard = () => {
   return (
     <div>
       Test Admin
-      <UserList />
+      <CreateDoctor></CreateDoctor>
     </div>
   )
 }
--- frontend/src/Component/Admin/CreateDoctor.jsx
+++ frontend/src/Component/Admin/CreateDoctor.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import styles from '../../Style/CreateDoctor.module.css'
 
 const CreateDoctor = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Add New Doctor</h1>
+            <form action="">
+                
+            </form>
+        </div>
+    )
 }
 
 export default CreateDoctor
