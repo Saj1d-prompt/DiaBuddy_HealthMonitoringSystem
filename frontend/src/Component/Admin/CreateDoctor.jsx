61f5b31b17bc76d20eb6e153dc5ec7ee7a5fe0c1
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-06 15:33:54 +0600

    Connected FE and BE API

--- frontend/src/Component/Admin/CreateDoctor.jsx
+++ frontend/src/Component/Admin/CreateDoctor.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import styles from '../../Style/CreateDoctor.module.css'
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const CreateDoctor = () => {
-    const { handleSubmit, register } = useForm();
+    const { handleSubmit, register, reset } = useForm();
+    const [error, setError] = useState(null);
+    const onSubmit = async (data) => {
+        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+        await fetch(`${import.meta.env.VITE_API_KEY}/admin/addDoctor`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                accept: 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                if (result.status === 200) {
+                    setError("Doctor added successfully.");
+                    reset();
+                    setTimeout(() => {
+                        setError(null);
+                    }, 2000);
+                } else {
+                    setError("Registration failed. Please try again with Valid Credentials.");
+                    setTimeout(() => {
+                        setError(null);
+                    }, 2000);
+                }
+            })
+    }
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Add New Doctor</h1>
+            {error && <p className={styles.error}>{error}</p>}
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className={styles.inputGroup}>
                     <label htmlFor="name">Name</label>
