2ad9230ff812ab2be3068833d506a9813ee2352f
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-06 14:18:05 +0600

    Starting Frontend

---
+++ frontend/src/Component/Admin/CreateDoctor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../Style/CreateDoctor.module.css'
+
+const CreateDoctor = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateDoctor
