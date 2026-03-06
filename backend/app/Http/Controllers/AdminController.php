25aed3b2c92e7388e20267a94c85a647fbc5fe6b
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-06 14:14:32 +0600

    Backend API of Create Doctor Completed

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    //
+    function addDoctor(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email',
+            'date_of_birth' => 'required|date',
+            'password' => 'required|string|min:5',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => 'Validation failed',
+                'data' => $validate->errors()
+            ]);
+        }
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->date_of_birth = $request->date_of_birth;
+        $user->password = Hash::make($request->password);
+        $user->role = 'doctor';
+        $user->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Doctor added successfully',
+            'data' => $user
+        ]);
+    }
 }
