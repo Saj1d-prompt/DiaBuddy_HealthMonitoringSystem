4c052f0677d2b01122249f8dc3f8bb3ad44e4e65
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-07 15:54:55 +0600

    Updated Model Class

--- backend/app/Models/Doctor.php
+++ backend/app/Models/Doctor.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'department',
+        'specialization',
+        'licenseNumber',
+        'yearOfExperience',
+        'profBio',
+        'highestDegree',
+        'medicalSchool',
+        'gradYear',
+        'awards',
+        'phoneNumber',
+        'clinicName',
+        'clinicAddress',
+        'consultationHours',
+        'fee',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
