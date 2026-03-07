14f233d7ab94523f59fff88d726864d1426251b9
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-07 15:48:02 +0600

    Database Schema updated

--- backend/database/migrations/2026_03_06_074656_create_doctors_table.php
+++ backend/database/migrations/2026_03_06_074656_create_doctors_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string(column: 'department');
+            $table->string(column: 'specialization')->nullable();
+            $table->string(column: 'licenseNumber')->unique();
+            $table->string(column: 'yearOfExperience');
+            $table->string(column: 'profBio');
+
+            $table->string(column: 'highestDegree');
+            $table->string(column: 'medicalSchool');
+            $table->string(column: 'gradYear');
+            $table->string(column: 'awards')->nullable();
+
+            $table->string(column: 'phoneNumber');
+            $table->string(column: 'clinicName');
+            $table->string(column: 'clinicAddress');
+            $table->string(column: 'consultationHours');
+            $table->string(column: 'fee');
+
+            $table->foreignId(column: 'user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
