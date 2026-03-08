7eceb3d074531daf44de79964077675d7aff61dc
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-08 14:39:29 +0600

    Validated attributes in BE

--- backend/app/Http/Controllers/DoctorController.php
+++ backend/app/Http/Controllers/DoctorController.php
@@ -1,10 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    //
+    public function updateDoctorProfile(Request $request)
+    {
+        $validatedData =Validator::make($request->all(), [
+            'department' => 'required|string',
+            'specialization' => 'nullable|string',
+            'licenseNumber' => 'required|string',
+            'yearOfExperience' => 'nullable|integer',
+            'profBio' => 'nullable|string',
+            'highestDegree' => 'required|string',
+            'medicalSchool' => 'required|string',
+            'gradYear' => 'required|integer',
+            'awards' => 'nullable|string',
+            'phoneNumber' => 'required|string',
+            'clinicName' => 'nullable|string',
+            'clinicAddress' => 'nullable|string',
+            'consultationHours' => 'required|string',
+            'fee' => 'required|numeric',
+        ]);
+
+        if ($validatedData->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validatedData->errors()
+            ], 400);
+        }
+
+    }
 }
