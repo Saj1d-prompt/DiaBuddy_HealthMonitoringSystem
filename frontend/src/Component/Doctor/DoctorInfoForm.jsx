019333c24dc8aa4202716af302123131fbe1e29e
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-08 15:27:26 +0600

    Modified Attributes

--- frontend/src/Component/Doctor/DoctorInfoForm.jsx
+++ frontend/src/Component/Doctor/DoctorInfoForm.jsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 const DoctorInfoForm = () => {
     const {register, handleSubmit} = useForm();
     const onSubmit = (data) => {
-        
+
     }
     return (
         <div className={styles.container}>
@@ -14,7 +14,7 @@ const DoctorInfoForm = () => {
                 <div className={styles.section}>
                     <h2>Professional Details</h2>
                     <label htmlFor="department">Department</label>
-                    <select id="department" name="department" required >
+                    <select id="department" {...register("department")} name="department" required >
                         <option value="">Select a department</option>
                         <option value="cardiology">Endocrinology</option>
                         <option value="neurology">Diabetology</option>
@@ -24,37 +24,37 @@ const DoctorInfoForm = () => {
                         <option value="cardiology">Cardiology</option>
                     </select>
                     <label htmlFor="specialty">Specialization</label>
-                    <input type="text" id="specialty" name="specialty" required />
+                    <input type="text" id="specialty" name="specialty" {...register("specialization")} required />
                     <label htmlFor="License">License Number</label>
-                    <input type="text" id="License" name="License" required />
+                    <input type="text" id="License" name="License" {...register("licenseNumber")} required />
                     <label htmlFor="Experience">Years of Experience</label>
-                    <input type="number" id="Experience" name="Experience" required />
+                    <input type="number" id="Experience" name="Experience" {...register("Experience")} required />
                     <label htmlFor="profBio">Professional Biography</label>
-                    <textarea id="profBio" name="profBio" />
+                    <textarea id="profBio" name="profBio" {...register("profBio")} />
                 </div>
                 <div className={styles.section}>
                     <h2>Education & Background</h2>
                     <label htmlFor="HighestDegree">Highest Degree</label>
-                    <input type="text" id="HighestDegree" name="HighestDegree" />
+                    <input type="text" id="HighestDegree" name="HighestDegree" {...register("highestDegree")} />
                     <label htmlFor="MedicalSchool">Medical School</label>
-                    <input type="text" id="MedicalSchool" name="MedicalSchool" />
+                    <input type="text" id="MedicalSchool" name="MedicalSchool" {...register("medicalSchool")} />
                     <label htmlFor="GraduationYear">Graduation Year</label>
-                    <input type="number" id="GraduationYear" name="GraduationYear" />
+                    <input type="number" id="GraduationYear" name="GraduationYear" {...register("gradYear")} />
                     <label htmlFor="Awards">Awards or Special Achievements(Optional)</label>
-                    <textarea id="Awards" name="Awards"></textarea>
+                    <textarea id="Awards" name="Awards" {...register("awards")}></textarea>
                 </div>
                 <div className={styles.section}>
                     <h2>Clinic & Contact Details</h2>
                     <label htmlFor="phoneNum">Phone Number</label>
-                    <input type="text" id="phoneNum" name="phoneNum" />
+                    <input type="text" id="phoneNum" name="phoneNum" {...register("phoneNumber")} />
                     <label htmlFor="clinicName">Clinic Name</label>
-                    <input type="text" id="clinicName" name="clinicName" />
+                    <input type="text" id="clinicName" name="clinicName" {...register("clinicName")} />
                     <label htmlFor="clinicAddress">Full Clinic Address</label>
-                    <textarea id="clinicAddress" name="clinicAddress" />
+                    <textarea id="clinicAddress" name="clinicAddress" {...register("clinicAddress")} />
                     <label htmlFor="consultationHours">Consultation Hours</label>
-                    <textarea id="consultationHours" name="consultationHours" />
+                    <textarea id="consultationHours" name="consultationHours" {...register("consultationHours")} />
                     <label htmlFor="fee">Consultation Fee</label>
-                    <input type="text" name="fee" id="fee" />
+                    <input type="text" name="fee" id="fee" {...register("fee")} />
                 </div>
                 <button type="submit" className={styles.submitBtn}>Submit Information</button>
             </form>
