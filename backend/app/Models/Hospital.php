23e2a002ad0de16e3d2ec5721076891e2898b4f4
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-09 15:09:04 +0600

    Model Class updated

--- backend/app/Models/Hospital.php
+++ backend/app/Models/Hospital.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hospital extends Model
 {
-    //
+    protected $fillable = [
+        'type',
+        'name',
+        'license_number',
+        'address',
+        'phone',
+        'email',
+        'is_active',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
