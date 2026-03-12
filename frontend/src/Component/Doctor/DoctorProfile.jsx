cf33285f6af0496ec3db318f385eb4ab1843b582
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-12 15:14:51 +0600

    Modified Doctor profile

--- frontend/src/Component/Doctor/DoctorProfile.jsx
+++ frontend/src/Component/Doctor/DoctorProfile.jsx
@@ -16,20 +16,28 @@ const DoctorProfile = () => {
                     <h3 className={styles.sectionTitle}>Academic & Professional</h3>
                     <div className={styles.grid}>
                         <div className={styles.formGroup}>
+                            <label htmlFor="">Department</label>
+                            <input type="text" name='department' value="Endocrinology" readOnly />
+                        </div>
+                        <div className={styles.formGroup}>
+                            <label htmlFor="">Specialization</label>
+                            <input type="text" name='specialization' value="Diabetes" readOnly />
+                        </div>
+                        <div className={styles.formGroup}>
                             <label htmlFor="">Medical License Number</label>
                             <input type="text" name='license_number' value="123456789" readOnly />
                         </div>
                         <div className={styles.formGroup}>
-                            <label htmlFor="">Specialization</label>
-                            <input type="text" name='specialization' value="Endocrinology" readOnly />
+                            <label htmlFor="">Years of Experience</label>
+                            <input type="text" name='experience' value="10 years" readOnly />
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="">Highest Degree</label>
                             <input type="text" name='highest_degree' value="MBBS" readOnly />
                         </div>
                         <div className={styles.formGroup}>
-                            <label htmlFor="">Years of Experience</label>
-                            <input type="text" name='experience' value="10 years" readOnly />
+                            <label htmlFor="">Medical School</label>
+                            <input type="text" name='medical_school' value="Johns Hopkins University" readOnly />
                         </div>
                     </div>
                 </form>
