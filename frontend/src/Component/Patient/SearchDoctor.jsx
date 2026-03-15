fdcb73fbed3964b3c7cafaccc5e523ce6443203d
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-15 14:04:53 +0600

    Added header

--- frontend/src/Component/Patient/SearchDoctor.jsx
+++ frontend/src/Component/Patient/SearchDoctor.jsx
@@ -4,7 +4,14 @@ import styles from '../../Style/SearchDoctor.module.css'
 const SearchDoctor = () => {
   return (
     <div>
-      
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1>Find a Diabetes Specialist</h1>
+          <div className={styles.searchBox}>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
