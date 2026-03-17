255da6bad978cc3eec80570c936dfbe54ed8405a
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-17 14:20:44 +0600

    Added doctor quer

--- backend/app/Http/Controllers/PatientController.php
+++ backend/app/Http/Controllers/PatientController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    //
+    public function searchDoctor(Request $request)
+    {
+        $query = Doctor::with(['user'=>function($q){
+            $q->select('id', 'name');
+        }]);
+    }
 }
