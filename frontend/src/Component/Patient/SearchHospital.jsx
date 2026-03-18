6898b66059580c7533c5c1d9b8fe6859a54cf7cd
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-18 14:58:44 +0600

    Added Health care center type

--- frontend/src/Component/Patient/SearchHospital.jsx
+++ frontend/src/Component/Patient/SearchHospital.jsx
@@ -5,7 +5,14 @@ const SearchHospital = () => {
     <div>
       <div className={styles.container}>
         <div className={styles.header}>
-          <h1>Find a Health Center</h1>
+            <h1>Find a Health Center</h1>
+            <div className={styles.searchBox}>
+                <select>
+                    <option value="">Select Health Center Type</option>
+                    <option value="hospital">Hospital</option>
+                    <option value="diagnosis_center">Diagnosis Center</option>
+                </select>
+            </div>
         </div>
       </div>
     </div>
