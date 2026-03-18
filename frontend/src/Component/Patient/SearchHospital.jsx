29bab59d30c677fdb74af74c77501dbd25aaa07c
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-18 14:55:23 +0600

    Header added

--- frontend/src/Component/Patient/SearchHospital.jsx
+++ frontend/src/Component/Patient/SearchHospital.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const SearchHospital = () => {
   return (
     <div>
-      
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1>Find a Health Center</h1>
+        </div>
+      </div>
     </div>
   )
 }
