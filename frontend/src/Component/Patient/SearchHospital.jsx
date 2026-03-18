804b9e04fa9e47f165f66adb10ab0d3a133a6a27
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-18 14:59:22 +0600

    Formatted

--- frontend/src/Component/Patient/SearchHospital.jsx
+++ frontend/src/Component/Patient/SearchHospital.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 const SearchHospital = () => {
-  return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.header}>
-            <h1>Find a Health Center</h1>
-            <div className={styles.searchBox}>
-                <select>
-                    <option value="">Select Health Center Type</option>
-                    <option value="hospital">Hospital</option>
-                    <option value="diagnosis_center">Diagnosis Center</option>
-                </select>
+    return (
+        <div>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <h1>Find a Health Center</h1>
+                    <div className={styles.searchBox}>
+                        <select>
+                            <option value="">Select Health Center Type</option>
+                            <option value="hospital">Hospital</option>
+                            <option value="diagnosis_center">Diagnosis Center</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default SearchHospital
