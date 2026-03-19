3337f5c70363385aa6fc628b28d082afb48c88b0
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-19 15:20:59 +0600

    Appointment Model class  Attributes

--- backend/app/Models/Appointment.php
+++ backend/app/Models/Appointment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $table = 'appointments';
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'appointment_date',
+        'start_time',
+        'end_time',
+        'status',
+    ];
 }
