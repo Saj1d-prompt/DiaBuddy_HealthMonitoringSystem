3546dcc9cd966c35b90585a6369e4e81d6a3dd96
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-19 15:21:25 +0600

    Modified Appointment Model class

--- backend/app/Models/Appointment.php
+++ backend/app/Models/Appointment.php
@@ -15,4 +15,12 @@ class Appointment extends Model
         'end_time',
         'status',
     ];
+        public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
 }
