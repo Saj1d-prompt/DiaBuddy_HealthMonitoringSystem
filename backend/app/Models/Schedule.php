077c6cd1605cda46b8705240f4235378a692d6cb
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-19 15:19:35 +0600

    Schedule Model class

--- backend/app/Models/Schedule.php
+++ backend/app/Models/Schedule.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    //
+    protected $table = 'schedules';
+    protected $fillable = [
+        'user_id',
+        'day',
+        'start_time',
+        'end_time',
+    ];
 }
