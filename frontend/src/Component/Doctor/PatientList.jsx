d09b7457c7520d556fbf33caeff68d6e6643a9be
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-25 14:49:37 +0600

    Formatted Document

--- frontend/src/Component/Doctor/PatientList.jsx
+++ frontend/src/Component/Doctor/PatientList.jsx
@@ -2,37 +2,37 @@ import React from 'react'
 import styles from '../../Style/PatientList.module.css'
 
 const PatientList = () => {
-  return (
-    <div>
-      <div className={styles.container}>
-        <h2>Upcoming Appointments</h2>
-        <div className={styles.appointmentList}>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Patient Name</th>
-                        <th>Date of Birth</th>
-                        <th>Age</th>
-                        <th>Gender</th>
-                        <th>Appointment Date</th>
-                        <th>Appointment Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>John Smith</td>
-                        <td>01/01/2000</td>
-                        <td>26</td>
-                        <td>Male</td>
-                        <td>25/03/2026</td>
-                        <td>10:00 AM - 10:30 AM</td>
-                    </tr>
-                </tbody>
-            </table>
+    return (
+        <div>
+            <div className={styles.container}>
+                <h2>Upcoming Appointments</h2>
+                <div className={styles.appointmentList}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Patient Name</th>
+                                <th>Date of Birth</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                                <th>Appointment Date</th>
+                                <th>Appointment Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>John Smith</td>
+                                <td>01/01/2000</td>
+                                <td>26</td>
+                                <td>Male</td>
+                                <td>25/03/2026</td>
+                                <td>10:00 AM - 10:30 AM</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default PatientList
