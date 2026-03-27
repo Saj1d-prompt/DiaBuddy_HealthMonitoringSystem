781d751e5d503f92dd3b5d95b8c9d30ddfa83d92
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-27 10:52:36 +0600

    Added Dummy Patient Information

--- frontend/src/Component/Doctor/PatientFacilities.jsx
+++ frontend/src/Component/Doctor/PatientFacilities.jsx
@@ -14,6 +14,15 @@ const PatientFacilities = () => {
             <p>Phone Number: 123-456-7890</p>
             <p>Address: Dhaka Bangladesh</p>
           </div>
+          <div className={styles.medicalInfo}>
+            <div className={styles.medicalInfoGrid}>
+              <p>Height: 180cm</p>
+              <p>Weight: 80kg</p>
+              <p>BMI: 24.7</p>
+            </div>
+          </div>
+          Blood Group: A+
+          Diabetes Type: Type 2 
         </aside>
       </div>
     </div>
