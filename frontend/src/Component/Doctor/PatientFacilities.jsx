207d1b011a930a5efc3297d527c6c969cd72cd05
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-03-27 10:46:43 +0600

    Starting Patient Facilities page

--- frontend/src/Component/Doctor/PatientFacilities.jsx
+++ frontend/src/Component/Doctor/PatientFacilities.jsx
@@ -4,7 +4,14 @@ import styles from '../../Style/PatientFacilities.module.css'
 const PatientFacilities = () => {
   return (
     <div>
-      
+      <div>
+        <aside>
+          <button>← Back to Patients List</button>
+          <div className="patientInfo">
+            <h1>John Smith</h1>
+          </div>
+        </aside>
+      </div>
     </div>
   )
 }
