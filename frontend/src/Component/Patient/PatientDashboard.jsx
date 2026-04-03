aa822b4d01b09f00944def080810fcfd0f05bad3
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-03 15:08:54 +0600

    Added logo to header

--- frontend/src/Component/Patient/PatientDashboard.jsx
+++ frontend/src/Component/Patient/PatientDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BloodSugarReadings from './BloodSugarReading'
 import ViewPrescription from './ViewPrescription'
+import PatientNavbar from './PatientNavbar'
 
 const PatientDashboard = () => {
   return (
     <div>
+      <PatientNavbar />
         Test Dashboard
         <button onClick={() => window.location.href = '/uploadreports'}>
           Upload Reports
--- frontend/src/Component/Patient/PatientNavbar.jsx
+++ frontend/src/Component/Patient/PatientNavbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from '../../Style/Navbar.module.css'
+import { Link } from 'react-router-dom'
+import logo from '../Images/DiaBuddy.png'
 
 const PatientNavbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <Link to="/"><img className={styles.logo} src={logo} alt="DiaBuddy Logo" /></Link>
+            </div>
+            <nav>
+
+            </nav>
+        </div>
+    )
 }
 
 export default PatientNavbar
