265cfc34840b6ecc87d731b89a54985052374466
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-03 17:30:52 +0600

    Added logo and logout

--- frontend/src/Component/Admin/AdminDashboard.jsx
+++ frontend/src/Component/Admin/AdminDashboard.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import UserList from './UserList';
 import CreateDoctor from './CreateDoctor';
 import AddHospital from './AddHospital';
+import AdminNavbar from './AdminNavbar';
+
 
 const AdminDashboard = () => {
   return (
     <div>
+      <AdminNavbar />
       Test Admin
       <AddHospital />
       <CreateDoctor />
--- frontend/src/Component/Admin/AdminNavbar.jsx
+++ frontend/src/Component/Admin/AdminNavbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styles from '../../Style/Navbar.module.css'
+import { Link } from 'react-router-dom'
+import logo from '../Images/DiaBuddy.png'
+import { AuthContext } from '../../Context/AuthContext'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AdminNavbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const handleLogout = () => {
+        
+    }
+    return (
+        <div>
+            <nav className={styles.navbar}>
+                <div className={styles["nav-container"]}>
+                    <Link to="/"><img className={styles.logo} src={logo} alt="DiaBuddy Logo" /></Link>
+
+                    <div>
+                        <button onClick={handleLogout} className={`${styles.btn} ${styles["btn-primary"]}`}>Logout</button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default AdminNavbar
