5c64935cb52fd1767966f901b48b891a44a6c28a
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-03 16:38:08 +0600

    Logout functionality

--- frontend/src/Component/Doctor/DoctorNavbar.jsx
+++ frontend/src/Component/Doctor/DoctorNavbar.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import styles from '../../Style/Navbar.module.css'
 import { Link } from 'react-router-dom'
 import logo from '../Images/DiaBuddy.png'
+import { AuthContext } from '../../Context/AuthContext'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const DoctorNavbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
     const handleLogout = () => {
-        
+        logout();
+        navigate('/');
     }
     return (
         <div>
             <nav className={styles.navbar}>
                 <div className={styles["nav-container"]}>
                     <Link to="/"><img className={styles.logo} src={logo} alt="DiaBuddy Logo" /></Link>
-
                     
                     <div>
                         <button onClick={handleLogout} className={`${styles.btn} ${styles["btn-primary"]}`}>Logout</button>
