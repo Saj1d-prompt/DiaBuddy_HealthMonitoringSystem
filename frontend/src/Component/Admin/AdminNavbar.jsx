eb56aa4c5e93c2bbe50ef0f730c241a6ac439c9a
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-03 17:32:16 +0600

    Added logout functionality

--- frontend/src/Component/Admin/AdminNavbar.jsx
+++ frontend/src/Component/Admin/AdminNavbar.jsx
@@ -7,15 +7,18 @@ import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const AdminNavbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
     const handleLogout = () => {
-        
+        logout();
+        navigate('/');
     }
     return (
         <div>
             <nav className={styles.navbar}>
                 <div className={styles["nav-container"]}>
                     <Link to="/"><img className={styles.logo} src={logo} alt="DiaBuddy Logo" /></Link>
-
+                    
                     <div>
                         <button onClick={handleLogout} className={`${styles.btn} ${styles["btn-primary"]}`}>Logout</button>
                     </div>
