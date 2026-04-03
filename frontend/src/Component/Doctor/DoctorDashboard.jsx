22b2762d75a8ccf3595e8677ec973f091a40b550
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-03 16:36:34 +0600

    Added the logo and logout

--- frontend/src/Component/Doctor/DoctorDashboard.jsx
+++ frontend/src/Component/Doctor/DoctorDashboard.jsx
@@ -3,10 +3,12 @@ import DoctorInfoForm from './DoctorInfoForm'
 import DoctorProfile from './DoctorProfile'
 import Schedule from './Schedule'
 import PatientList from './PatientList'
+import DoctorNavbar from './DoctorNavbar'
 
 const DoctorDashboard = () => {
   return (
     <div>
+      <DoctorNavbar/>
       Test Dashboard
       <PatientList/>
     </div>
--- frontend/src/Component/Doctor/DoctorNavbar.jsx
+++ frontend/src/Component/Doctor/DoctorNavbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import styles from '../../Style/Navbar.module.css'
 import { Link } from 'react-router-dom'
+import logo from '../Images/DiaBuddy.png'
 
 const DoctorNavbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const handleLogout = () => {
+        
+    }
+    return (
+        <div>
+            <nav className={styles.navbar}>
+                <div className={styles["nav-container"]}>
+                    <Link to="/"><img className={styles.logo} src={logo} alt="DiaBuddy Logo" /></Link>
+
+                    
+                    <div>
+                        <button onClick={handleLogout} className={`${styles.btn} ${styles["btn-primary"]}`}>Logout</button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default DoctorNavbar
