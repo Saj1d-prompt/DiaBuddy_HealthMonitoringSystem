5cb5f54e03ccae50be44dd468e0fe2aefb74ce22
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-03 21:37:24 +0600

    Added role base navbar

--- frontend/src/Component/Common/Layout.jsx
+++ frontend/src/Component/Common/Layout.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../Context/AuthContext';
+import AdminNavbar from '../Admin/AdminNavbar';
+import DoctorNavbar from '../Doctor/DoctorNavbar';
+import PatientNavbar from '../Patient/PatientNavbar';
 
 const Layout = ({ children }) => {
     const { user } = useContext(AuthContext);
@@ -8,6 +13,15 @@ const Layout = ({ children }) => {
         return <Outlet />;
     }
     const userRole = user?.role;
+    if(userRole === 'admin') {
+        return (<AdminNavbar />)
+    }else if(userRole === 'doctor') {
+        return (<DoctorNavbar />)
+    }else if(userRole === 'patient') {
+        return (<PatientNavbar />)
+    }else {
+        return null;
+    }
   return (
     <div>
       
