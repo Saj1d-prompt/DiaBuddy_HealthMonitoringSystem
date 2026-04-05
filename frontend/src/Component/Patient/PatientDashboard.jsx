88bee2605b674daec8c4c05c0680724b4e127f24
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-05 11:59:21 +0600

    Added classnames

--- frontend/src/Component/Patient/PatientDashboard.jsx
+++ frontend/src/Component/Patient/PatientDashboard.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import BloodSugarReadings from './BloodSugarReading'
 import ViewPrescription from './ViewPrescription'
 import PatientNavbar from './PatientNavbar'
+import styles from '../../Style/PatientDashboard.module.css'
 
 const PatientDashboard = () => {
   return (
     <div>
-      <div className="personalInfo">
+      <div className={styles.personalInfo}>
         <h2>Mr. X</h2>
-        <div className="InfoFlex">
+        <div className={styles.InfoFlex}>
           <p>Age: 45</p>
           <p>Gender: Male</p>
           <p>address: Basundhara R/A, Dhaka, Bangladesh</p>
         </div>
-        <div className="InfoFlex">
+        <div className={styles.InfoFlex}>
           <p>Weight: 70 kg</p>
           <p>Height: 175 cm</p>
           <p>BMI: 22.9</p>
