6937dc75a0b9186b00fb129ac18f6f7cfa78e84e
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-08 16:28:31 +0600

    Made Info fetching API backend

--- backend/app/Http/Controllers/DashboardControllerr.php
+++ backend/app/Http/Controllers/DashboardControllerr.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BloodSugarReading;
+use App\Models\Person;
+use Illuminate\Support\Facades\DB;
 
 class DashboardControllerr extends Controller
 {
@@ -17,6 +19,26 @@ class DashboardControllerr extends Controller
         return response()->json([
             'status' => 200,
             'data' => $bsr
+        ], 200);
+    }
+
+    public function getPatientInfo(Request $request)
+    {
+        $user = DB::table('users')
+            ->join('person', 'users.id', '=', 'person.user_id')
+            ->select(
+                'users.name',
+                'person.gender',
+                'person.address',
+                'person.weight',
+                'person.height',
+                'person.blood_group')
+            ->where('users.id', $request->user()->id)
+            ->first();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $user
         ], 200);
     }
 }
