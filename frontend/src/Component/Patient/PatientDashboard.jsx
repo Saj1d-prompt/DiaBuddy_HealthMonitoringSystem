98726fee198ad3ec7587c3c4c66c9b23197aec8c
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-09 16:03:52 +0600

    Stored data in state

--- frontend/src/Component/Patient/PatientDashboard.jsx
+++ frontend/src/Component/Patient/PatientDashboard.jsx
@@ -133,7 +133,10 @@ const PatientDashboard = () => {
           'Authorization': `Bearer ${userInfo.token}`
         }
       });
-      
+      const data = await response.json();
+      if (data.status == 200) {
+        setDoctors(data.data);
+      }
     }catch(error){
       console.error("Error fetching doctors:", error);
     }
