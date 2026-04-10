9b6951f18fa4f0e217bd8275ca473ad05a3c1120
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-10 21:46:08 +0600

    Added Dummy data for stats

--- frontend/src/Component/Admin/AdminDashboard.jsx
+++ frontend/src/Component/Admin/AdminDashboard.jsx
@@ -8,7 +8,18 @@ const AdminDashboard = () => {
   return (
     <div>
       <div className="statGrid">
-        
+        <div className="statCard">
+          <h3>Total Doctors</h3>
+          <p>50</p>
+        </div>
+        <div className="statCard">
+          <h3>Total Hospitals</h3>
+          <p>20</p>
+        </div>
+        <div className="statCard">
+          <h3>Total Patients</h3>
+          <p>200</p>
+        </div>
       </div>
     </div>
   )
