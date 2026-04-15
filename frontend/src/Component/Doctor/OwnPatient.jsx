c54ff892274f3ba001c64650c2a9f0f24ff250e2
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-04-15 10:26:58 +0600

    Table heading Added

--- frontend/src/Component/Doctor/OwnPatient.jsx
+++ frontend/src/Component/Doctor/OwnPatient.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 import styles from '../../Style/PatientList.module.css'
 
 const OwnPatient = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className={styles.container}>
+                <h2>Patients that Visited You</h2>
+                <div className={styles.appointmentList}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Patient Name</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                                <th>Contact Number</th>
+                                <th>Blood Group</th>
+                                <th>Diabetes Type</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default OwnPatient
